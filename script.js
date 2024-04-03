1e5dbf991c395a632eb71db3f53f3243327abef5
Shenith <shenithbandara@gmail.com>
2024-04-03 14:42:21 +0530

    change bg and character

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let move_speed = 8, gravity = 0.5;
+let move_speed = 8, gravity = 0.4;
 let bird = document.querySelector('.bird');
 let img = document.getElementById('bird-1');
 let sound_point = new Audio('sounds effect/point.mp3');
